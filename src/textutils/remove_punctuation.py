90c2b8b775120040ddfc17d543bcd1c3913ab750
sharathandres51-eng <sharathandres51@gmail.com>
2025-10-28 18:44:43 +0100

    create a method for removing punctuation

---
+++ src/textutils/remove_punctuation.py
@@ -0,0 +1,7 @@
+import string
+
+def remove_punctuation(text):
+    # Create an empty string to store the cleaned text
+    cleaned_text = ""
+    # Loop through each character in the input text
+    for ch in text:
