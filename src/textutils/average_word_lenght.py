70d50eba34af73a77815c514108e1beeef8c73aa
selfede <sel.fede@gmail.com>
2025-10-28 18:38:04 +0100

    feat(avg_word_length): completing the code here!

--- src/textutils/average_word_lenght.py
+++ src/textutils/average_word_lenght.py
@@ -9,3 +9,7 @@ def average_word_length(text: str) -> float:
 
     if not words:
         return 0.0
+    
+    total_length = sum(len(word) for word in words)
+    avg_length = total_length / len(words)
+    return round(avg_length, 2)
