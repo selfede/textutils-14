1ca2eda2c002d5a167b48ae446dee921d256c1aa
Moriarty <floriannix4@gmail.com>
2025-10-28 17:08:11 +0100

    Great code guys, implementing unique word extraction here

--- src/textutils/unique_words.py
+++ src/textutils/unique_words.py
@@ -4,6 +4,7 @@ def unique_words(text: str):
     Return a sorted list of unique, case-insensitive words from the text.
     """
     text = text.lower()
+    words = re.findall(r'\b[a-z0-9]+\b', text)
 
 
     
