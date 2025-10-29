003ab4a8c98e2075046debf4fe00ecf6fde97f87
Moriarty <floriannix4@gmail.com>
2025-10-29 14:18:59 +0100

    Moving the slugify feature to core

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,1 +1,15 @@
-from .slugify import slugify
+import re
+
+def slugify(text):
+    text = text.lower()
+    text = text.replace(' ', '-')
+
+    result = ''
+    for char in text:
+        if char.isalnum() or char == '-':
+            result += char
+
+    result = re.sub(r'-+', '-', result)
+    result = result.strip('-')
+
+    return result
