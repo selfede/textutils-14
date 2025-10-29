0b004ee8c4adb86bf94b7b6bcb941bd98c99d091
selfede <sel.fede@gmail.com>
2025-10-29 15:24:45 +0100

    moved the feature to core

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,16 +1,16 @@
-import textutils.average_word_lenght as awl
+import textutils.core as c
 
 def test_simple_words():
     # this fucntion tests with simple words
-    result = awl.average_word_length("python git")
+    result = c.average_word_length("python git")
     assert result == 4.5
 
 def test_different_lengths():
     # Test with different word lengths: "I" (1) + "love" (4) + "Python" (6) = 11 / 3 = 3.67
-    result = awl.average_word_length("good job team")
+    result = c.average_word_length("good job team")
     assert result == 3.67
 
 def test_only_punctuation():
-    # Test with only punctuation - should return 0.0
-    result = awl.average_word_length("!@#$%^&*()")
+    # Test with only punctuation: should return 0.0
+    result = c.average_word_length("!@#$%^&*()")
     assert result == 0.0
