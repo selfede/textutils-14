e93e19d28f4145c0b766093c23a3a06f6058fdcf
selfede <sel.fede@gmail.com>
2025-10-29 14:29:21 +0100

    moving word lengths feature to core

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,16 +1,16 @@
-import textutils.word_lengths as wl
+import textutils.core as c
 
 def test_simple_words():
     # this tests checks basic words
-    result = wl.word_lengths("hello world")
+    result = c.word_lengths("hello world")
     assert result == {"hello": 5, "world": 5}
 
 def test_different_lengths():
     # this function tests for with words of different lengths
-    result = wl.word_lengths("great work team")
+    result = c.word_lengths("great work team")
     assert result == {"great": 5, "work": 4, "team": 4}
 
 def test_with_punctuation():
     # this tests that punctuation is included in the word length
-    result = wl.word_lengths("great work!")
+    result = c.word_lengths("great work!")
     assert result == {"great": 5, "work!": 5}
